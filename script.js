352f22ff3591f7abb4f38a21169ca559f74796e8
odielijah <odielijah456@gmail.com>
2024-07-18 00:36:54 +0100

    slider

---
+++ script.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay: {
+        delay: 7000,
+        disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
